bc79522296fc3b58bfb292603ad62173dd15211a
zyaamd <Sadeeshamanamperi2009@gmail.com>
2025-12-22 22:48:12 -0800

    Create info.js

---
+++ commond/info.js
@@ -0,0 +1,13 @@
+module.exports = {
+    command: '.info',
+    description: 'Show bot and developer info',
+    reply: `BOT INFO
+----------------------------
+Name       : ZYAA MD BOT
+Developer  : sadeesha_nethsara
+Version    : 1.0.0
+Library    : whatsapp-web.js
+Status     : Running
+
+Type .menu to see all commands.`
+};
