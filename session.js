22cc46c19ce5dbfcc6b0db77c0a4e57f3bf7d0d8
zyaamd <Sadeeshamanamperi2009@gmail.com>
2025-12-22 23:59:57 -0800

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ZYAA_MD=38160223c2f0a597",
+SESSION_ID: process.env.SESSION_ID || "ZYAA_MD=70c75e59239bc4837",
 PORT: process.env.PORT || "8000"
 };
