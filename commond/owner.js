a5afb1d3361c7e734ff38e6d426385cc234e47d1
zyaamd <Sadeeshamanamperi2009@gmail.com>
2025-12-22 22:49:18 -0800

    Create owner.js

---
+++ commond/owner.js
@@ -0,0 +1,12 @@
+module.exports = {
+    command: '.owner',
+    description: 'Show bot owner info',
+    reply: `OWNER INFO
+----------------------------
+Name       : sadeesha_nethsara
+Contact    : wa.me/94742127877
+Role       : Developer / Bot Owner
+Status     : Active
+
+Type .menu to see all commands.`
+};
