34bba535dec0a6521e3959d7778764af97235466
zyaamd <Sadeeshamanamperi2009@gmail.com>
2025-12-22 22:43:49 -0800

    Create system.js

---
+++ commond/system.js
@@ -0,0 +1,12 @@
+module.exports = {
+    command: '.system',
+    description: 'Show system info',
+    reply: `SYSTEM INFO
+--------------------
+OS       : Node.js
+Library  : whatsapp-web.js
+Status   : Running
+Uptime   : Active Now
+
+Type .menu to see all commands`
+};
